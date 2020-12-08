e067689d2efded2ad9da2455b01b16198ff00c06
denmercs <denmercs@gmail.com>
2020-12-07 18:21:17 -0600

    added more notes about mongodb

--- notes.js
+++ notes.js
@@ -2,16 +2,24 @@
 
 //commands on shell
 
-show dbs
-use flights
-db.flightData.insertOne({
-       {
-        "departureAirport": "MUC",
-        "arrivalAirport": "SFO",
-        "distance": 12000,
-        "intercontinental": true
+show dbs;
+use flights;
+db.flightData.insertOne(
+    {
+        departureAirport: "MUC",
+        arrivalAirport: "SFO",
+        aircraft: "Airbus A380",
+        distance: 12000,
+        intercontinental: true
+    },
+    {
+        departureAirport: "LHR",
+        arrivalAirport: "TXL",
+        aircraft: "Airbus A320",
+        distance: 12000,
+        intercontinental: true
     }
-})
+)
 
 db.flightData.find().pretty()
 
@@ -23,16 +31,46 @@ insertMany(data, options)
 
 // READ
 find(filter, options)
-findOne(filter, options)ß
+findOne(filter, options)
+
+db.flightData.find({departureAirport: "MUC"})
+db.flightData.find({distance: {$gt: 10000}}).pretty();
+db.flightData.find({distance: {$gt: 10000}}).pretty();
+db.flightData.find({distance: {$gt: 900}}).toArray();
+db.flightData.find({distance: {$gt: 900}}).forEach(data => {printjson(data)});
 
 // UPDATE
 updateOne(filter, data, options)
 updateMany(filter, data, options)
 update({filter, data, options}) // --> this will update everything on the data and just leave what you set as parameter
 
+db.flightData.updateOne({distance: 12000}, {$set: {marker: "delete"}})
+db.flightData.updateMany({}, {$set: {marker: "toDelete"}})
+db.flightData.insertMany([
+    {
+        departureAirport: "MUC",
+        arrivalAirport: "SFO",
+        aircraft: "Airbus A380",
+        distance: 12000,
+        intercontinental: true
+    },
+    {
+        departureAirport: "LHR",
+        arrivalAirport: "TXL",
+        aircraft: "Airbus A320",
+        distance: 12000,
+        intercontinental: true
+    }
+])
+
+db.flightData.updateOne({_id: ObjectId("1253251235w54asf34134")}, {$set: {delayed: true}})
+db.flightData.update({_id: ObjectId("1253251235w54asf34134")}, {delayed: true}) // --> this will overwrite the existing object and return only id and delayed
+
+
 // DELETE
 deleteOne(filter, options)
 deleteMany(filter, options)
+db.flightData.deleteMany({marker: "toDelete"})
 
  
 db.passengers.find().forEach((passengerData) => {
